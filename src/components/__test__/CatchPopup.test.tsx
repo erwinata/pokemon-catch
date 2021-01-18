b4070a3dcfdf764a18ed2c627d448ad2c5208c0d
Erwin Winata <erwinata96@gmail.com>
2021-01-18 12:51:15 +0700

    add assertion when catch failed

--- src/components/__test__/CatchPopup.test.tsx
+++ src/components/__test__/CatchPopup.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
-import { fireEvent, render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CatchPopup from "components/CatchPopup";
 import { AppContext, IAppState } from "context/context";
@@ -20,7 +20,6 @@ test("catch pokemon and give nickname", async () => {
 
   //faking results of catch
   const fakeRandomBoolean = getRandomBoolean as jest.MockedFunction<typeof getRandomBoolean>;
-  fakeRandomBoolean.mockReturnValue(true);
   //faking timeout so no need to wait
   const fakeRandomTimeout = getRandomTimeout as jest.MockedFunction<typeof getRandomTimeout>;
   fakeRandomTimeout.mockReturnValue(0);
@@ -31,17 +30,15 @@ test("catch pokemon and give nickname", async () => {
     </AppContext.Provider>
   );
 
-  // await waitForElementToBeRemoved(() => screen.getByRole("img"));
+  fakeRandomBoolean.mockReturnValue(false);
+  await waitFor(() => expect(fakeRandomBoolean).toHaveBeenCalledTimes(1));
 
-  // screen.debug();
+  //show try again button when failed
+  expect(screen.queryByText("Try Again")).toBeTruthy();
 
-  // //show try again button when failed
-  // expect(screen.queryByText("Try again")).toBeTruthy();
-
-  // fakeRandomBoolean.mockReturnValue(true);
-  // fireEvent.click(screen.getByText("Try again"));
-
-  await waitForElementToBeRemoved(() => screen.getByRole("img"));
+  fakeRandomBoolean.mockReturnValue(true);
+  fireEvent.click(screen.getByText("Try Again"));
+  await waitFor(() => expect(fakeRandomBoolean).toHaveBeenCalledTimes(2));
 
   //show form
   expect(screen.queryByText(/Nickname/)).toBeTruthy();
